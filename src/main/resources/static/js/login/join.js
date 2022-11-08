5416f3b53d003e0541aba960621c691d73a18559
nage24 <n0rkjdx0@naver.com>
2022-11-08 09:13:35 +0900

    2022-11-06 login&join page

---
+++ src/main/resources/static/js/login/join.js
@@ -0,0 +1,8 @@
+
+const toggleContents = document.querySelectorAll("join-toggle");
+
+toggleContents.onclick = () => {
+    toggleContents.classList.toggle('active');
+}
+
+// active 일 경우, 약관 상세 보여줌. 
