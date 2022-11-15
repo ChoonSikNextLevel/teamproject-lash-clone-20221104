219359da11923c099c891cb52cfb7c004345fb80
seung530 <tmd530@naver.com>
2022-11-15 13:34:31 +0900

    1115 상품등록 service 수정/작성 중

--- src/main/java/com/lash/lashClone/service/admin/ProductServiceImpl.java
+++ src/main/java/com/lash/lashClone/service/admin/ProductServiceImpl.java
@@ -1,10 +1,15 @@
 package com.lash.lashClone.service.admin;
 
 import com.lash.lashClone.domain.Product;
+import com.lash.lashClone.domain.ProductImg;
 import com.lash.lashClone.dto.admin.ProductRegistReqDto;
 import com.lash.lashClone.repository.admin.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,13 +22,32 @@ public class ProductServiceImpl implements ProductService {
 
         int result = 0;
 
+        List<MultipartFile> files = productRegistReqDto.getFiles();
+        List<ProductImg> productImgs = null;
+
         Product product = productRegistReqDto.toProduct();
         result = productRepository.saveProduct(product);
+
+        if(files != null) {
+            int productId = product.getProduct_id();
+            productImgs = getProductImgs(files, productId);
+        }
 
         if(result == 0) {
             return false;
         }
 
         return true;
+    }
+
+    private List<ProductImg> getProductImgs(List<MultipartFile> files, int productId)throws Exception {
+        List<ProductImg> productImgs = new ArrayList<ProductImg>();
+
+        files.forEach(file -> {
+            String origin_name
+        });
+
+
+        return productImgs;
     }
 }
