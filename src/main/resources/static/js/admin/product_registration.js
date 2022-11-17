802adc899406f2459c13918673f40b54f96826f4
nage24 <>
2022-11-17 13:48:46 +0900

    2022-11-17 상품 등록 페이지 이미지 삭제 기능 완료

--- src/main/resources/static/js/admin/product_registration.js
+++ src/main/resources/static/js/admin/product_registration.js
@@ -6,101 +6,110 @@ const nameUl = document.querySelector(".uploaded_files_name");
 const fileUl = document.querySelector(".cvf_uploaded_files");
 
 productImgInput.onchange = () => {
-
-
-    let productImgs = document.getElementById("product-imgs");
-    let files = productImgs.files;
-    let file;
-
-    let imgArray = Array.from(files);
-    console.log(imgArray);
-
-    nameUl.innerHTML = ``;
-    fileUl.innerHTML = ``;
-
-    for(let i = 0; i < files.length; i++) {
-
-        file = files[i];
-        
-        nameUl.innerHTML += `
-                <li style="background-color: #b7d5bb; border: 1px solid gray; margin: 2px 0; border-radius: 2px; width: 50%;">
-                    선택된 파일[${i}]: ${file.name}
-                </li>
-        `;
-
-        /* img 불러오기 */
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-            fileUl.innerHTML += `
-                <li>
-                    <img class="product-img" src="${e.target.result}" style="width: 140px; height: 140px;">
-                    <i class="fa-solid fa-circle-minus delete-btn"></i>
-                </li>
-            `;
-        }
-
-        setTimeout(() => {reader.readAsDataURL(files[i])}, i * 100);
-    }
-
-    alert("잘 대나");
-    
-}
-
-
-
-registButton.onclick = () => {
-
-    const formData = new FormData(document.querySelector("form"));
-
-    let productForm = new FormData();
-    let productImgs = document.getElementById("product-imgs");
-
-    productForm.append("category", productInput[0].value);
-	productForm.append("name", productInput[1].value);
-	productForm.append("colorCode", productInput[2].value); // color_code 로 쓰면 못 알아 봄 !! dto에 있는 이름 대로 . . 
-	productForm.append("color", productInput[3].value);
-    productForm.append("price", productInput[4].value);
-    productForm.append("productFeatures", productInput[5].value);
-    productForm.append("description", productInput[6].value);
-    //productForm.append("returnPolicy", productInput[7].value);
-
-/* =============== 이미지 form append ==================== */	
-    let files = productImgs.files;
-    let file;
-
-    for(let i = 0; i < files.length; i++) {
-        productForm.append("productImgs", productImgs.files[i]);
-    }
-    
-
-
-    //deleteBtn.onclick
-
-    addProduct(productForm);
-    alert("동작완료");
+  imgReader();
 };
 
+function imgReader() {
+  const productImgs = document.getElementById("product-imgs");
+  const files = productImgs.files;
+
+  nameUl.innerHTML = ``;
+  fileUl.innerHTML = ``;
+
+  for (let i = 0; i < files.length; i++) {
+    nameUl.innerHTML += `
+                    <li style="background-color: #b7d5bb; border: 1px solid gray; margin: 2px 0; border-radius: 2px; width: 50%;">
+                        선택된 파일[${i}]: ${files[i].name}
+                    </li>
+            `;
+
+    /* img 불러오기 */
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      fileUl.innerHTML += `
+                    <li>
+                        <img class="product-img" src="${e.target.result}" style="width: 140px; height: 140px;">
+                        <i class="fa-solid fa-circle-minus delete-btn"></i>
+                    </li>
+                `;
+
+      deleteImg(files);
+    };
+
+    setTimeout(() => {
+      reader.readAsDataURL(files[i]);
+    }, i * 100);
+  }
+}
+
+function deleteImg(files) {
+  let imgArray = Array.from(files);
+  const deleteBtns = document.querySelectorAll(".delete-btn");
+  const dataTransfer = new DataTransfer();
+  console.log("deleteBtns?");
+  console.log(deleteBtns);
+
+  deleteBtns.forEach((deleteBtn, index) => {
+    deleteBtn.onclick = () => {
+      imgArray.splice(index, 1);
+
+      imgArray.forEach((file) => {
+        dataTransfer.items.add(file);
+      });
+
+      document.querySelector("#product-imgs").files = dataTransfer.files;
+
+      imgReader();
+    };
+  });
+}
+
+registButton.onclick = () => {
+  const formData = new FormData(document.querySelector("form"));
+
+  let productForm = new FormData();
+  let productImgs = document.getElementById("product-imgs");
+
+  productForm.append("category", productInput[0].value);
+  productForm.append("name", productInput[1].value);
+  productForm.append("colorCode", productInput[2].value); // color_code 로 쓰면 못 알아 봄 !! dto에 있는 이름 대로 . .
+  productForm.append("color", productInput[3].value);
+  productForm.append("price", productInput[4].value);
+  productForm.append("productFeatures", productInput[5].value);
+  productForm.append("description", productInput[6].value);
+  //productForm.append("returnPolicy", productInput[7].value);
+
+  /* =============== 이미지 form append ==================== */
+
+  let files = productImgs.files;
+
+  for (let i = 0; i < files.length; i++) {
+    productForm.append("productImgs", productImgs.files[i]);
+  }
+
+  addProduct(productForm);
+  alert("동작완료");
+};
 
 function addProduct(productForm) {
-	$.ajax({
-			async: false,
-			type: "post",
-			url: "/api/admin/product/registration",
-			
-			enctype: "multipart/form-data",
-			contentType: false,
-			processData: false,
-			
-			data: productForm,
-			dataType: "json",
-			success: (response) => {
-				alert("상품 등록 완료");
-			},
-			error: (error) => {
-				alert("상품 등록 실패");
-				console.log(error);
-			}
-		
-	});
+  $.ajax({
+    async: false,
+    type: "post",
+    url: "/api/admin/product/registration",
+
+    enctype: "multipart/form-data",
+    contentType: false,
+    processData: false,
+
+    data: productForm,
+    dataType: "json",
+    success: (response) => {
+      alert("상품 등록 완료");
+    },
+    error: (error) => {
+      alert("상품 등록 실패");
+      console.log(error);
+    },
+  });
 }
