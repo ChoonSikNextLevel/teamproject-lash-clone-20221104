a0057494ea2a0445e10d8bb30f63213fea92edd3
nage24 <>
2022-11-18 17:48:20 +0900

    2022-11-18 product_update 이미지 수정 삭제 post

--- src/main/resources/static/js/admin/product_update.js
+++ src/main/resources/static/js/admin/product_update.js
@@ -1,47 +1,129 @@
 let product = JSON.parse(localStorage.getItem("product"));
 let productId = product.productId;
 
+const productImgInput = document.querySelector("#product-imgs");
+const fileUl = document.querySelector(".cvf_uploaded_files");
+
+productImgInput.onchange = () => {
+  addImg();
+};
 
 // 항목별로 등록되어 있는 value 값 불러오기
 function getOriginValues(product) {
-    const updateInput = document.querySelectorAll(".update-input");
-    
-    //  select value 값 불러오기
-    const updateSelect = document.querySelector("#update-select");
-    const selectLength = updateSelect.options.length;
-    let optionValue = product.category;
+  const updateInput = document.querySelectorAll(".update-input");
 
-    // 기존 select 값과 일치하는 value에 seleted 옵션을 줘라
-    for(let i = 0; i < selectLength; i++) {
-        if(updateSelect.options[i].value == optionValue) {
-            updateSelect.options[i].selected = true;
-        }
+  //  select value 값 불러오기
+  const updateSelect = document.querySelector("#update-select");
+  const selectLength = updateSelect.options.length;
+  let optionValue = product.category;
+
+  // 기존 select 값과 일치하는 value에 seleted 옵션을 줘라
+  for (let i = 0; i < selectLength; i++) {
+    if (updateSelect.options[i].value == optionValue) {
+      updateSelect.options[i].selected = true;
     }
+  }
 
-    updateInput[1].innerHTML = `
+  updateInput[1].innerHTML = `
     <input id="member_id" class="product-input" name="member_id" type="text" value="${product.name}"></input>
     `;
 
-    updateInput[2].innerHTML = `
+  updateInput[2].innerHTML = `
     <input id="member_id" class="product-input" name="member_id" type="text" value="${product.colorCode}"></input>
     `;
 
-    updateInput[3].innerHTML = `
+  updateInput[3].innerHTML = `
     <input id="member_id" class="product-input" name="member_id" type="text" value="${product.color}"></input>
     `;
 
-    updateInput[4].innerHTML = `
+  updateInput[4].innerHTML = `
     <input id="member_id" class="product-input" name="member_id" type="text" value="${product.price}"></input>
     `;
 
-    updateInput[5].innerHTML = `
+  updateInput[5].innerHTML = `
     <textarea class="product-features-ex product-input" placeholder="">${product.productFeatures}</textarea>
     `;
 
-    updateInput[6].innerHTML = `
+  updateInput[6].innerHTML = `
     <textarea class="product-features-ex product-input" placeholder="">${product.description}</textarea>
     `;
 
+  const productImgsArray = product.productImgs;
+  loadImg(productImgsArray);
+}
+
+function loadImg(productImgsArray) {
+  fileUl.innerHTML = ``;
+
+  productImgsArray.forEach((img, index) => {
+    fileUl.innerHTML += `
+            <li>
+                <img class="product-img" src="/image/product/${productImgsArray[index].img_name}" style="width: 140px; height: 140px;">
+                <i class="fa-solid fa-circle-minus delete-btn"></i>
+            </li>
+        `;
+
+    deleteOriginImg(productImgsArray);
+  });
+}
+
+function deleteOriginImg(imgArray) {
+  const deleteBtns = document.querySelectorAll(".delete-btn");
+
+  deleteBtns.forEach((deleteBtn, index) => {
+    deleteBtn.onclick = () => {
+      console.log(imgArray[index].img_name);
+
+      imgArray.splice(index, 1);
+
+      loadImg(imgArray);
+    };
+  });
+}
+
+function addImg() {
+  const productImgs = document.getElementById("product-imgs");
+  const addFiles = productImgs.files;
+
+  for (let i = 0; i < addFiles.length; i++) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      fileUl.innerHTML += `
+                    <li>
+                        <img class="product-img" src="${e.target.result}" style="width: 140px; height: 140px;">
+                        <i class="fa-solid fa-circle-minus delete-btn"></i>
+                    </li>
+                `;
+
+      deleteImg(addFiles);
+    };
+
+    setTimeout(() => {
+      reader.readAsDataURL(addFiles[i]);
+    }, i * 100);
+  }
+}
+
+function deleteImg(files) {
+  let imgArray = Array.from(files);
+
+  const deleteBtns = document.querySelectorAll(".delete-btn");
+  const dataTransfer = new DataTransfer();
+
+  deleteBtns.forEach((deleteBtn, index) => {
+    deleteBtn.onclick = () => {
+      imgArray.splice(index, 1);
+
+      imgArray.forEach((file) => {
+        dataTransfer.items.add(file);
+      });
+
+      document.querySelector("#product-imgs").files = dataTransfer.files;
+
+      addImg();
+    };
+  });
 }
 
 // 수정 후 등록 시 아래 데이터 전송(post)
@@ -49,48 +131,48 @@ function getOriginValues(product) {
 const updateButton = document.querySelector(".registration-button");
 
 updateButton.onclick = () => {
-    alert("클릭됐다");
-    updateProduct();
-}
+  updateProduct();
+};
 
 function updateProduct() {
-    const productInput = document.querySelectorAll(".product-input");
-    
-    let productForm = new FormData();
-    // let productImgs = document.getElementById("product-imgs");
-    
-    productForm.append("productId", productId);
-    productForm.append("category", productInput[0].value);
-    productForm.append("name", productInput[1].value);
-    productForm.append("colorCode", productInput[2].value); // color_code 로 쓰면 못 알아 봄 !! dto에 있는 이름 대로 . .
-    productForm.append("color", productInput[3].value);
-    productForm.append("price", productInput[4].value);
-    productForm.append("productFeatures", productInput[5].value);
-    productForm.append("description", productInput[6].value);
+  const productInput = document.querySelectorAll(".product-input");
 
-    $.ajax({
-        async: false,
-        type: "post",
-        url: "/api/admin/product/update",
-        enctype: "multipart/form-data",
-        contentType: false,
-        processData: false,
-        data: productForm,
-        dataType: "json",
-        success: (response) => {
-            alert("상품 수정 완료");
-            location.reload();
-        },
-        error: (error) => {
-            alert("상품 수정 실패");
-            console.log(error);
-        }
-    });
+  let productForm = new FormData();
+  // let productImgs = document.getElementById("product-imgs");
+
+  productForm.append("productId", productId);
+  productForm.append("category", productInput[0].value);
+  productForm.append("name", productInput[1].value);
+  productForm.append("colorCode", productInput[2].value); // color_code 로 쓰면 못 알아 봄 !! dto에 있는 이름 대로 . .
+  productForm.append("color", productInput[3].value);
+  productForm.append("price", productInput[4].value);
+  productForm.append("productFeatures", productInput[5].value);
+  productForm.append("description", productInput[6].value);
+  //productForm.append("",);
+
+  $.ajax({
+    async: false,
+    type: "post",
+    url: "/api/admin/product/update",
+    enctype: "multipart/form-data",
+    contentType: false,
+    processData: false,
+    data: productForm,
+    dataType: "json",
+    success: (response) => {
+      alert("상품 수정 완료");
+      location.reload();
+    },
+    error: (error) => {
+      alert("상품 수정 실패");
+      console.log(error);
+    },
+  });
 }
 
 window.onload = () => {
-    // let product = JSON.parse(localStorage.getItem("product"));
-    // let productId = product.productId;
-    console.log(product);
-    getOriginValues(product);
-}
+  // let product = JSON.parse(localStorage.getItem("product"));
+  // let productId = product.productId;
+  console.log(product);
+  getOriginValues(product);
+};
