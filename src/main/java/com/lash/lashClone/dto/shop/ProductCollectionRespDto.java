9406eec7347d37f6ff08377e5224c6c51acfcb5e
seung530 <tmd530@naver.com>
2022-11-21 12:35:26 +0900

    11/21 collection 페이지 구성 중

---
+++ src/main/java/com/lash/lashClone/dto/shop/ProductCollectionRespDto.java
@@ -0,0 +1,19 @@
+package com.lash.lashClone.dto.shop;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ProductCollectionRespDto {
+
+    private int productId;
+    private String category;
+    private String name;
+    private String colorCode;
+    private String color;
+    private int price;
+    private String imgName;
+    private int productTotalCount;
+
+}
