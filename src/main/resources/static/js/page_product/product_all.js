97835358fbf74540eeddcd75d60295992e9ff714
seung530 <tmd530@naver.com>
2022-11-22 13:37:28 +0900

    11/22 collection load 완료, 이름별 묶기 작업 중

--- src/main/resources/static/js/page_product/product_all.js
+++ src/main/resources/static/js/page_product/product_all.js
@@ -34,6 +34,8 @@ function getCollection() {
             alert("COLLECTION 불러오기 성공");
             console.log("응답 : ", response.data);
             loadCollection(response.data);
+            
+            groupByName(response.data);
         },
         error: (error) => {
             alert("COLLECTION 불러오기 실패");
@@ -43,78 +45,67 @@ function getCollection() {
 }
 
 
-// const productImg = document.querySelector(".product-list-img");
-// const productName = document.querySelector(".product-name");
-// const productPrice = document.querySelector(".product-price");
-
-const productList = document.querySelector(".product-list");
-
-function loadCollection(collection) {
-    collection.forEach(product => {
-        console.log("제품 : ", product);
-        console.log("카테고리 : ", product.category);
-               
-        // if(product.category == category) {
-
-        //     productList.innerHTML += `
-        //         <div class="product-list-img">
-        //             <div class="product-hunter hunter-c1">
-        //                 <img src="/image/product/${product.img_name}">
-        //             </div>
-        //         </div>
-        //         <div class="product-info-box">
-        //             <div class="product-info-text">
-        //                 <p class="product-name">${product.name}</p>
-        //                 <p class="product-price">KRW ${product.price}</p>
-        //             </div>
-        //             <div class="product-info-color hunter-color">
-        //                 <img src="/static/images/product_img/product_color_img/HUNTER_c1.jpg">
-        //                 <img src="/static/images/product_img/product_color_img/HUNTER_c2.jpg">
-        //                 <img src="/static/images/product_img/product_color_img/HUNTER_c3.jpg">
-        //                 <img src="/static/images/product_img/product_color_img/HUNTER_c4.jpg">
-        //             </div>
-        //         </div>
-        //     `;
-        // }
-
-    });
-
+// 이름으로 그룹 묶기
+function groupByName(responseData) {
+    const nameArray = responseData.reduce( (array, curV) => {
+        const {name} = curV;
+        
+        if (array[name]) {
+           array[name].push(curV);
+        }else {
+           array[name] = [curV];
+        }
+        return array;               
+        }
+        , { });
     
-
-    
-
-
-
-
+    console.log(nameArray);
+    return nameArray;
 }
 
 
 
+const productContainer = document.querySelector(".product-container");
 
+function loadCollection(collection) {
+    console.log("컬렉션 : " , collection);
+    
+    collection.forEach(product => {
+        const productName = product.name;
+
+        console.log("이름 : " , productName);
+        console.log("제품 : ", product);
+        console.log("카테고리 : ", product.category);
+        console.log("이미지 : ", product.img_name);
+                
+            productContainer.innerHTML += `
+                <div class="product-list">
+                    <div class="product-list-img">
+                        <div class="product-hunter hunter-c1">
+                            <a href="/products/product/${product.name}/${product.color_code}">  
+                                <img src="/image/product/${product.img_name}">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="product-info-box">
+                        <div class="product-info-text">
+                            <p class="product-name">${product.name}</p>
+                            <p class="product-price">KRW ${product.price}</p>
+                        </div>
+                        <div class="product-info-color hunter-color">
+                            <img src="/static/images/product_img/product_color_img/HUNTER_c1.jpg">
+                            <img src="/static/images/product_img/product_color_img/HUNTER_c2.jpg">
+                            <img src="/static/images/product_img/product_color_img/HUNTER_c3.jpg">
+                            <img src="/static/images/product_img/product_color_img/HUNTER_c4.jpg">
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+}
 
 
 window.onload = () => {
     getCollection();
+    // groupByName();
 }
-
-
-
-
-
-{/* <div class="product-list-img">
-                    <div class="product-hunter hunter-c1">
-                        <img src="/image/product/${product.img_name}">
-                    </div>
-                </div>
-                <div class="product-info-box">
-                    <div class="product-info-text">
-                        <p class="product-name">${product.name}</p>
-                        <p class="product-price">KRW ${product.price}</p>
-                    </div>
-                    <div class="product-info-color hunter-color">
-                        <img src="/static/images/product_img/product_color_img/HUNTER_c1.jpg">
-                        <img src="/static/images/product_img/product_color_img/HUNTER_c2.jpg">
-                        <img src="/static/images/product_img/product_color_img/HUNTER_c3.jpg">
-                        <img src="/static/images/product_img/product_color_img/HUNTER_c4.jpg">
-                    </div>
-                </div> */}
